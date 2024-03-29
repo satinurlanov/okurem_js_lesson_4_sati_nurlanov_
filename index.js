c3945a4009f9b170117e4d0df8a2513f2afc6b4d
satar nurlan uulu <umarfaaruq2019@gmail.com>
2024-03-29 17:21:25 +0600

    lesson4

---
+++ index.js
@@ -0,0 +1,66 @@
+// switch (car) {
+//     case ("bmw"):
+//         console.log('bmw');
+//         break;
+//     case "toyota":
+//         console.log('toyota');
+//         break;
+//     case "gelik":
+//         console.log('gelik');
+//         break;
+//     case "kia":
+//         console.log('kia');
+//         break;
+//     case "fit":
+//         console.log('fit');
+//         break;
+//     case "lexsus":
+//         console.log('leksus');
+//         break;
+//     case "matis":
+//         console.log('matis');
+//         break;
+//     default:
+//         console.log(car);
+
+// }
+// const car = "bmw";
+// switch (car) {
+//     case ("bmw"):
+//         console.log('bmw');
+//     case "toyota":
+//         console.log('toyota');
+//     case "gelik":
+//         console.log('gelik');
+//     case "kia":
+//         console.log('kia');
+//     case "fit":
+//         console.log('fit');
+//     case "lexsus":
+//         console.log('leksus');
+//     case "matis":
+//         console.log('matis');
+//     default:
+//         console.log(car);
+
+// }
+
+// ! - не эмес
+// && - и жана
+//  ||  - или же
+
+// const  number = parseInt(prompt ('enter a number from 1 to 100',''))
+// const  number = +(prompt ('enter a number from 1 to 100',''))
+// if (number < 1 || number > 100) {
+//     alert('number is invalid');
+// } else {
+//     alert('success');
+// }
+
+
+// Array - massiv
+
+const arrayOAnimals = ['cat', 'dog','sheep','fish','lion'];
+
+// console.log(arrayOAnimals[4]);
+console.log(arrayOAnimals.length);
